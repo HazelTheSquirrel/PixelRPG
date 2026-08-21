390970e7169ec0c921251047696393f09db27281
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-21 18:06:24 +0200

    Inject central dialog engine into reception behavior

--- src/main/java/de/pixelrpg/rpg/npc/behavior/ReceptionBehavior.java
+++ src/main/java/de/pixelrpg/rpg/npc/behavior/ReceptionBehavior.java
@@ -1,5 +1,6 @@
 package de.pixelrpg.rpg.npc.behavior;
 
+import de.pixelrpg.rpg.dialogue.DialogueEngine;
 import de.pixelrpg.rpg.dialogue.ReceptionDialog;
 import de.pixelrpg.rpg.npc.NpcBehavior;
 import de.pixelrpg.rpg.npc.NpcType;
@@ -8,11 +9,12 @@ import de.pixelrpg.rpg.player.PlayerProfileManager;
 import org.bukkit.entity.Player;
 
 public final class ReceptionBehavior implements NpcBehavior {
-
     private final PlayerProfileManager profileManager;
+    private final DialogueEngine dialogueEngine;
 
-    public ReceptionBehavior(PlayerProfileManager profileManager) {
+    public ReceptionBehavior(PlayerProfileManager profileManager, DialogueEngine dialogueEngine) {
         this.profileManager = profileManager;
+        this.dialogueEngine = dialogueEngine;
     }
 
     @Override
@@ -22,6 +24,6 @@ public final class ReceptionBehavior implements NpcBehavior {
 
     @Override
     public void onInteract(Player player, RPGNpc npc) {
-        new ReceptionDialog(player, profileManager).open();
+        new ReceptionDialog(player, profileManager, dialogueEngine).open();
     }
 }
