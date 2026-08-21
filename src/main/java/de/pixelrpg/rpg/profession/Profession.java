545897a5380678b92fc1cd31601d573b9a11c011
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-21 16:49:18 +0200

    Add PixelRPG profession definitions

---
+++ src/main/java/de/pixelrpg/rpg/profession/Profession.java
@@ -0,0 +1,17 @@
+package de.pixelrpg.rpg.profession;
+
+public enum Profession {
+    MINING,
+    WOODCUTTING,
+    HERBALISM,
+    SKINNING,
+    FISHING,
+    BLACKSMITHING,
+    LEATHERWORKING,
+    TAILORING,
+    ALCHEMY,
+    COOKING;
+
+    public static final int MIN_LEVEL = 1;
+    public static final int MAX_LEVEL = 100;
+}
