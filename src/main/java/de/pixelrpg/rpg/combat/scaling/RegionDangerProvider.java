d8ed765362ed7092dfab925ba91c0ac2ac4ce43a
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-21 13:55:59 +0200

    Replace region rank bounds with level bounds

--- src/main/java/de/pixelrpg/rpg/combat/scaling/RegionDangerProvider.java
+++ src/main/java/de/pixelrpg/rpg/combat/scaling/RegionDangerProvider.java
@@ -1,12 +1,8 @@
-// src/main/java/de/pixelrpg/rpg/combat/scaling/RegionDangerProvider.java
 package de.pixelrpg.rpg.combat.scaling;
 
-import de.pixelrpg.rpg.core.Rank;
 import org.bukkit.Location;
 
 public interface RegionDangerProvider {
-
-    Rank getMinRank(Location location);
-
-    Rank getMaxRank(Location location);
+    int getMinLevel(Location location);
+    int getMaxLevel(Location location);
 }
