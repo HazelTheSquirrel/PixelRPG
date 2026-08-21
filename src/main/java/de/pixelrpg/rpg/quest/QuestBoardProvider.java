88cca40e685d1037b0c01e95f3bd5c7872812efc
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-21 19:51:55 +0200

    Remove obsolete quest board provider interface

--- src/main/java/de/pixelrpg/rpg/quest/QuestBoardProvider.java
+++
@@ -1,9 +0,0 @@
-// src/main/java/de/pixelrpg/rpg/quest/QuestBoardProvider.java
-package de.pixelrpg.rpg.quest;
-
-import org.bukkit.entity.Player;
-
-public interface QuestBoardProvider {
-
-    void openQuestBoard(Player player);
-}
