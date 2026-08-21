65a821eb85a118b5ea7f9d99276b740393e4fa7c
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-21 18:07:49 +0200

    Scope boss attack patterns to RPG targets

--- src/main/java/de/pixelrpg/rpg/boss/BossAttackPattern.java
+++ src/main/java/de/pixelrpg/rpg/boss/BossAttackPattern.java
@@ -1,12 +1,13 @@
-// src/main/java/de/pixelrpg/rpg/boss/BossAttackPattern.java
 package de.pixelrpg.rpg.boss;
 
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-public interface BossAttackPattern {
+import java.util.Collection;
 
+public interface BossAttackPattern {
     String id();
 
-    void execute(Plugin plugin, LivingEntity boss);
+    void execute(Plugin plugin, LivingEntity boss, Collection<Player> targets);
 }
