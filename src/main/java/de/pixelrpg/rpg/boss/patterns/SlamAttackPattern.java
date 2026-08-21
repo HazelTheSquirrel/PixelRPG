ea0cbad3a5539a4ffab0091398142a8f4251a29b
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-21 18:07:54 +0200

    Restrict boss slam to registered targets

--- src/main/java/de/pixelrpg/rpg/boss/patterns/SlamAttackPattern.java
+++ src/main/java/de/pixelrpg/rpg/boss/patterns/SlamAttackPattern.java
@@ -1,38 +1,41 @@
-// src/main/java/de/pixelrpg/rpg/boss/patterns/SlamAttackPattern.java
 package de.pixelrpg.rpg.boss.patterns;
 
 import de.pixelrpg.rpg.boss.BossAttackPattern;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.util.Vector;
 
-public final class SlamAttackPattern implements BossAttackPattern {
+import java.util.Collection;
 
+public final class SlamAttackPattern implements BossAttackPattern {
     @Override
     public String id() {
         return "SLAM";
     }
 
     @Override
-    public void execute(Plugin plugin, LivingEntity boss) {
+    public void execute(Plugin plugin, LivingEntity boss, Collection<Player> targets) {
         Location center = boss.getLocation();
         center.getWorld().spawnParticle(Particle.EXPLOSION, center, 3);
         center.getWorld().playSound(center, Sound.ENTITY_RAVAGER_ATTACK, 1.5f, 0.6f);
 
-        double radius = 6.0;
-        double damage = boss.getAttribute(org.bukkit.attribute.Attribute.ATTACK_DAMAGE) != null
-                ? boss.getAttribute(org.bukkit.attribute.Attribute.ATTACK_DAMAGE).getValue() * 1.5
+        double radiusSquared = 36.0;
+        double damage = boss.getAttribute(Attribute.ATTACK_DAMAGE) != null
+                ? boss.getAttribute(Attribute.ATTACK_DAMAGE).getValue() * 1.5
                 : 15.0;
 
-        for (org.bukkit.entity.Entity entity : boss.getNearbyEntities(radius, radius, radius)) {
-            if (entity instanceof Player player) {
-                player.damage(damage, boss);
-                Vector knockback = player.getLocation().toVector()
-                        .subtract(center.toVector()).normalize().multiply(1.5).setY(0.5);
+        for (Player player : targets) {
+            if (!player.isOnline() || player.getWorld() != boss.getWorld()) continue;
+            if (player.getLocation().distanceSquared(center) > radiusSquared) continue;
+            player.damage(damage, boss);
+            Vector offset = player.getLocation().toVector().subtract(center.toVector());
+            if (offset.lengthSquared() > 0.0001) {
+                Vector knockback = offset.normalize().multiply(1.5).setY(0.5);
                 player.setVelocity(knockback);
             }
         }
