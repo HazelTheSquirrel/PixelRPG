99c5ba556123314d483fcb438fa2e7ec678d9e50
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-23 10:01:19 +0200

    Resync NPCs for players after login

--- src/main/java/de/pixelrpg/rpg/npc/NpcChunkListener.java
+++ src/main/java/de/pixelrpg/rpg/npc/NpcChunkListener.java
@@ -3,6 +3,7 @@ package de.pixelrpg.rpg.npc;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 
@@ -14,11 +15,19 @@ public final class NpcChunkListener implements Listener {
         this.npcManager = npcManager;
     }
 
+    // Re-synchronizes NPC entities for a player after the client has finished joining.
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        npcManager.getPlugin().getServer().getScheduler().runTaskLater(npcManager.getPlugin(), () -> npcManager.resyncPlayer(event.getPlayer()), 2L);
+    }
+
+    // Restores NPCs when their chunk becomes loaded again.
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent event) {
         npcManager.handleChunkLoad(event.getChunk());
     }
 
+    // Removes NPC tracking entries when their chunk unloads.
     @EventHandler
     public void onChunkUnload(ChunkUnloadEvent event) {
         npcManager.handleChunkUnload(event.getChunk());
