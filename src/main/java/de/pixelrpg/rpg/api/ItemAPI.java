74983c2c68de9244d4f77334229e796b867e81fa
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-24 19:39:25 +0200

    Extend ItemAPI for concrete definitions and gearscore

--- src/main/java/de/pixelrpg/rpg/api/ItemAPI.java
+++ src/main/java/de/pixelrpg/rpg/api/ItemAPI.java
@@ -2,6 +2,7 @@
 package de.pixelrpg.rpg.api;
 
 import de.pixelrpg.rpg.item.ItemCategory;
+import de.pixelrpg.rpg.item.ItemDefinition;
 import de.pixelrpg.rpg.item.ItemRarity;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -12,6 +13,8 @@ public interface ItemAPI {
 
     Optional<ItemStack> createItem(Material material, ItemRarity rarity, int itemLevel);
 
+    Optional<ItemStack> createItem(String itemId);
+
     boolean isRPGItem(ItemStack item);
 
     boolean isGuildItem(ItemStack item);
@@ -21,4 +24,10 @@ public interface ItemAPI {
     Optional<ItemRarity> getRarity(ItemStack item);
 
     Optional<ItemCategory> getCategory(ItemStack item);
+
+    Optional<ItemDefinition> getDefinition(ItemStack item);
+
+    Optional<Integer> getRequiredLevel(ItemStack item);
+
+    Optional<Double> getGearscore(ItemStack item);
 }
