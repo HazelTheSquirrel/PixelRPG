6bea821e77775ea5ea5aef11244dd4fdc39b0239
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-24 19:14:35 +0200

    Persist story NPC dialogue state per player

--- src/main/java/de/pixelrpg/rpg/dialogue/StoryNpcDialogue.java
+++ src/main/java/de/pixelrpg/rpg/dialogue/StoryNpcDialogue.java
@@ -1,5 +1,6 @@
 package de.pixelrpg.rpg.dialogue;
 
+import de.pixelrpg.rpg.PixelRPGPlugin;
 import de.pixelrpg.rpg.npc.RPGNpc;
 import de.pixelrpg.rpg.player.PlayerProfileManager;
 import net.kyori.adventure.text.Component;
@@ -9,6 +10,7 @@ import org.bukkit.entity.Player;
 public final class StoryNpcDialogue {
     private final PlayerProfileManager profileManager;
     private final DialogueEngine dialogueEngine;
+    private final DialogueProgressStore progressStore;
 
     public StoryNpcDialogue(PlayerProfileManager profileManager) {
         this(profileManager, new DialogueEngine());
@@ -17,6 +19,15 @@ public final class StoryNpcDialogue {
     public StoryNpcDialogue(PlayerProfileManager profileManager, DialogueEngine dialogueEngine) {
         this.profileManager = profileManager;
         this.dialogueEngine = dialogueEngine;
+        this.progressStore = new DialogueProgressStore(PixelRPGPlugin.getInstance());
+        this.progressStore.load();
+    }
+
+    public StoryNpcDialogue(PlayerProfileManager profileManager, DialogueEngine dialogueEngine,
+                            DialogueProgressStore progressStore) {
+        this.profileManager = profileManager;
+        this.dialogueEngine = dialogueEngine;
+        this.progressStore = progressStore;
     }
 
     public void begin(Player player, RPGNpc npc) {
@@ -25,6 +36,7 @@ public final class StoryNpcDialogue {
             return;
         }
 
+        progressStore.markSeen(player.getUniqueId(), "story.npc." + npc.id());
         dialogueEngine.openUnavailable(
                 player,
                 "Geschichte",
