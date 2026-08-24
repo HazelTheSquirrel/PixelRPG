d89038108f0c25406b5144eba97b287aed07431a
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-24 16:52:43 +0200

    Add character stat type for progression API

---
+++ src/main/java/de/pixelrpg/rpg/stats/CharacterStatType.java
@@ -0,0 +1,14 @@
+package de.pixelrpg.rpg.stats;
+
+/** Public character-stat identifiers exposed by the progression/statistics API. */
+public enum CharacterStatType {
+    HP,
+    ARMOR,
+    MOVEMENT_SPEED,
+    REACH,
+    DAMAGE,
+    CRIT,
+    CRIT_DAMAGE,
+    LIFESTEAL,
+    ATTACK_POWER
+}
