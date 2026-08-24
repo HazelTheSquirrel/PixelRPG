7b4e85352de23d6080cb28fd2488ee9a8b4a816f
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-24 16:30:37 +0200

    refactor(combat): remove obsolete region danger scaling API

--- src/main/java/de/pixelrpg/rpg/combat/scaling/RegionDangerProvider.java
+++
@@ -1,8 +0,0 @@
-package de.pixelrpg.rpg.combat.scaling;
-
-import org.bukkit.Location;
-
-public interface RegionDangerProvider {
-    int getMinLevel(Location location);
-    int getMaxLevel(Location location);
-}
