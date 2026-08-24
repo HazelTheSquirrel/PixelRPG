b23d9e85883674b5211ef73ccf68b6f0c80965e0
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-24 15:59:35 +0200

    remove obsolete class and attribute API from player guild contract

--- src/main/java/de/pixelrpg/rpg/api/GuildAPI.java
+++ src/main/java/de/pixelrpg/rpg/api/GuildAPI.java
@@ -1,8 +1,5 @@
 package de.pixelrpg.rpg.api;
 
-import de.pixelrpg.rpg.player.PlayerAttribute;
-import de.pixelrpg.rpg.player.PlayerClass;
-
 import java.util.UUID;
 
 public interface GuildAPI {
@@ -11,13 +8,7 @@ public interface GuildAPI {
 
     int getLevel(UUID uuid);
 
-    PlayerClass getPlayerClass(UUID uuid);
-
-    boolean hasSelectedClass(UUID uuid);
-
     long getExperience(UUID uuid);
 
     void addExperience(UUID uuid, long amount);
-
-    int getAttributePoints(UUID uuid, PlayerAttribute attribute);
 }
