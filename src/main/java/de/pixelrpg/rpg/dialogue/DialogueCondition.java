4b6380f3d9d7acffbc1a60182d48886a0834cd04
HazelTheSquirrel <hazelgin01@gmail.com>
2026-08-24 19:13:50 +0200

    Add reusable dialogue condition API

---
+++ src/main/java/de/pixelrpg/rpg/dialogue/DialogueCondition.java
@@ -0,0 +1,19 @@
+package de.pixelrpg.rpg.dialogue;
+
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+@FunctionalInterface
+public interface DialogueCondition {
+    boolean test(Player player);
+
+    static DialogueCondition always() {
+        return player -> true;
+    }
+
+    static DialogueCondition of(Predicate<Player> predicate) {
+        return Objects.requireNonNull(predicate, "predicate")::test;
+    }
+}
