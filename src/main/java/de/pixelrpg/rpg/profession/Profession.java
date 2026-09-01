33ddb4bbf199f47187ab8b934c3c051e6c5a5bfc
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-01 07:55:35 +0200

    refactor: expand profession model to nine optional professions

--- src/main/java/de/pixelrpg/rpg/profession/Profession.java
+++ src/main/java/de/pixelrpg/rpg/profession/Profession.java
@@ -3,12 +3,17 @@ package de.pixelrpg.rpg.profession;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
-/** The four broad professions used by PixelRPG. */
+/** The nine optional professions available to every PixelRPG player. */
 public enum Profession {
     BLACKSMITH("Schmied", "Waffen, Rüstung, Werkzeuge und Metallverarbeitung", NamedTextColor.GRAY),
-    PROVISIONER("Versorger", "Nahrung, Fischerei, Landwirtschaft und Tierprodukte", NamedTextColor.GREEN),
-    ALCHEMIST("Alchemist", "Tränke, Reagenzien und pflanzliche Wirkstoffe", NamedTextColor.LIGHT_PURPLE),
-    SCHOLAR("Gelehrter", "Bücher, Karten, Verzauberungen und Wissensgegenstände", NamedTextColor.AQUA);
+    SCHOLAR("Gelehrter", "Bücher, Karten, Verzauberungen und Wissensgegenstände", NamedTextColor.AQUA),
+    FARMER("Landwirt", "Felder, Pflanzen und landwirtschaftliche Rohstoffe", NamedTextColor.GREEN),
+    COOK("Koch", "Nahrung, Mahlzeiten und verarbeitete Lebensmittel", NamedTextColor.GOLD),
+    TAILOR("Schneider", "Wolle, Leder, Stoffe und textile Verarbeitung", NamedTextColor.LIGHT_PURPLE),
+    ALCHEMIST("Alchemist", "Tränke, Reagenzien und pflanzliche Wirkstoffe", NamedTextColor.DARK_PURPLE),
+    MASON("Steinmetz", "Stein, Ziegel und die Verarbeitung von Baublöcken", NamedTextColor.DARK_GRAY),
+    FISHERMAN("Fischer", "Angeln und Fisch als Rohstoff für die Küche", NamedTextColor.BLUE),
+    WOODCUTTER("Holzfäller", "Holz, Stämme und die Verarbeitung von Holz", NamedTextColor.DARK_GREEN);
 
     public static final int MIN_LEVEL = 1;
     public static final int MAX_LEVEL = 100;
