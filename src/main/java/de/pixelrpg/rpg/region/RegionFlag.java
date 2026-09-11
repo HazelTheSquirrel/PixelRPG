2a7e409d854b278f3476855d715f610661e44de5
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-11 15:05:40 +0200

    Improve region flag presentation names

--- src/main/java/de/pixelrpg/rpg/region/RegionFlag.java
+++ src/main/java/de/pixelrpg/rpg/region/RegionFlag.java
@@ -1,14 +1,24 @@
 package de.pixelrpg.rpg.region;
 
 public enum RegionFlag {
-    PVP,
-    MONSTER_SPAWN,
-    BLOCK_BREAK,
-    BLOCK_PLACE,
-    FIRE_SPREAD,
-    LAVA_FLOW,
-    EXPLOSION,
-    CREEPER_EXPLOSION,
-    GHAST_FIREBALL,
-    ENDERMAN_GRIEF
+    PVP("PvP"),
+    MONSTER_SPAWN("Monster-Spawns"),
+    BLOCK_BREAK("Blockabbau"),
+    BLOCK_PLACE("Blockplatzierung"),
+    FIRE_SPREAD("Feuerausbreitung"),
+    LAVA_FLOW("Lavafluss"),
+    EXPLOSION("Explosionen"),
+    CREEPER_EXPLOSION("Creeper-Explosionen"),
+    GHAST_FIREBALL("Ghast-Feuerbälle"),
+    ENDERMAN_GRIEF("Enderman-Griefing");
+
+    private final String displayName;
+
+    RegionFlag(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String displayName() {
+        return displayName;
+    }
 }
