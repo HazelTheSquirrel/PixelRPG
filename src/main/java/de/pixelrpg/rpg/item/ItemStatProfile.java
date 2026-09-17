b51c2c7c5d7ac119d53d96d434187be7fefb3f74
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-17 22:23:10 +0200

    Add FOOD item stat profile

--- src/main/java/de/pixelrpg/rpg/item/ItemStatProfile.java
+++ src/main/java/de/pixelrpg/rpg/item/ItemStatProfile.java
@@ -1,9 +1,9 @@
-// src/main/java/de/pixelrpg/rpg/item/ItemStatProfile.java
 package de.pixelrpg.rpg.item;
 
 public enum ItemStatProfile {
     WEAPON,
     ARMOR,
     SHIELD,
-    TOOL
+    TOOL,
+    FOOD
 }
