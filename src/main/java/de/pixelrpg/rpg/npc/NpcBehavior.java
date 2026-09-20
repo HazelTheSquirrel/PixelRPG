5f1dae4586320606630b31694d3f52f308201157
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-21 01:53:26 +0200

    Expose dialogue context to NPC behaviors

--- src/main/java/de/pixelrpg/rpg/npc/NpcBehavior.java
+++ src/main/java/de/pixelrpg/rpg/npc/NpcBehavior.java
@@ -1,6 +1,7 @@
 // src/main/java/de/pixelrpg/rpg/npc/NpcBehavior.java
 package de.pixelrpg.rpg.npc;
 
+import de.pixelrpg.rpg.dialogue.DialogueContext;
 import org.bukkit.entity.Player;
 
 public interface NpcBehavior {
@@ -8,4 +9,8 @@ public interface NpcBehavior {
     NpcType type();
 
     void onInteract(Player player, RPGNpc npc);
+
+    default void onInteract(DialogueContext context) {
+        onInteract(context.player(), context.npc());
+    }
 }
