0a8aece54d7c09e31054ad5a6b00e5162700f8bc
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-21 01:51:00 +0200

    Add NPC categories for the dialogue-driven NPC foundation

---
+++ src/main/java/de/pixelrpg/rpg/npc/NpcCategory.java
@@ -0,0 +1,18 @@
+package de.pixelrpg.rpg.npc;
+
+public enum NpcCategory {
+    RESIDENT,
+    MERCHANT,
+    TRAVELER,
+    GUARD,
+    SCHOLAR,
+    SEEKER,
+    CRAFTSPERSON,
+    FARMER,
+    FISHERMAN,
+    STORY,
+    QUEST,
+    FACTION,
+    CHILD,
+    ELDER
+}
