a4d8a86e25b1aded6a3bb1c6419c44b12aafa393
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-21 03:40:29 +0200

    Decouple NPC category type from runtime functions

--- src/main/java/de/pixelrpg/rpg/npc/NpcType.java
+++ src/main/java/de/pixelrpg/rpg/npc/NpcType.java
@@ -2,47 +2,32 @@ package de.pixelrpg.rpg.npc;
 
 import net.kyori.adventure.text.format.NamedTextColor;
 
-import java.util.EnumSet;
-import java.util.Set;
-
 public enum NpcType {
 
-    RECEPTION(NamedTextColor.AQUA, NpcFunction.RECEPTION),
-    PROFESSION_BLACKSMITH(NamedTextColor.GRAY, NpcFunction.PROFESSION),
-    PROFESSION_SCHOLAR(NamedTextColor.AQUA, NpcFunction.PROFESSION),
-    PROFESSION_FARMER(NamedTextColor.GREEN, NpcFunction.PROFESSION),
-    PROFESSION_COOK(NamedTextColor.GOLD, NpcFunction.PROFESSION),
-    PROFESSION_TAILOR(NamedTextColor.LIGHT_PURPLE, NpcFunction.PROFESSION),
-    PROFESSION_ALCHEMIST(NamedTextColor.DARK_PURPLE, NpcFunction.PROFESSION),
-    PROFESSION_MASON(NamedTextColor.DARK_GRAY, NpcFunction.PROFESSION),
-    PROFESSION_FISHERMAN(NamedTextColor.BLUE, NpcFunction.PROFESSION),
-    PROFESSION_WOODCUTTER(NamedTextColor.DARK_GREEN, NpcFunction.PROFESSION),
-    QUEST(NamedTextColor.YELLOW, NpcFunction.QUEST),
-    SHOP(NamedTextColor.GREEN, NpcFunction.SHOP),
-    TRAVEL(NamedTextColor.LIGHT_PURPLE, NpcFunction.TRAVEL),
+    RECEPTION(NamedTextColor.AQUA),
+    PROFESSION_BLACKSMITH(NamedTextColor.GRAY),
+    PROFESSION_SCHOLAR(NamedTextColor.AQUA),
+    PROFESSION_FARMER(NamedTextColor.GREEN),
+    PROFESSION_COOK(NamedTextColor.GOLD),
+    PROFESSION_TAILOR(NamedTextColor.LIGHT_PURPLE),
+    PROFESSION_ALCHEMIST(NamedTextColor.DARK_PURPLE),
+    PROFESSION_MASON(NamedTextColor.DARK_GRAY),
+    PROFESSION_FISHERMAN(NamedTextColor.BLUE),
+    PROFESSION_WOODCUTTER(NamedTextColor.DARK_GREEN),
+    QUEST(NamedTextColor.YELLOW),
+    SHOP(NamedTextColor.GREEN),
+    TRAVEL(NamedTextColor.LIGHT_PURPLE),
     FILLER(NamedTextColor.WHITE),
-    STORY(NamedTextColor.GOLD, NpcFunction.STORY),
-    BANKER(NamedTextColor.DARK_GREEN, NpcFunction.BANK);
+    STORY(NamedTextColor.GOLD),
+    BANKER(NamedTextColor.DARK_GREEN);
 
     private final NamedTextColor color;
-    private final Set<NpcFunction> functions;
 
-    NpcType(NamedTextColor color, NpcFunction... functions) {
+    NpcType(NamedTextColor color) {
         this.color = color;
-        this.functions = functions.length == 0
-                ? Set.of()
-                : Set.copyOf(EnumSet.of(functions[0], functions));
     }
 
     public NamedTextColor getColor() {
         return color;
-    }
-
-    public Set<NpcFunction> functions() {
-        return functions;
-    }
-
-    public boolean supports(NpcFunction function) {
-        return functions.contains(function);
     }
 }
