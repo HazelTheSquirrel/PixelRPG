082f65002ed4e977889996021111b5bfc1ebece3
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-23 15:18:37 +0200

    rebuild: wire NPC runtime and persistence lifecycle

--- src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
+++ src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
@@ -8,10 +8,18 @@ import de.pixelrpg.rpg.economy.GuildCurrencyItemFactory;
 import de.pixelrpg.rpg.economy.GuildCurrencyPickupListener;
 import de.pixelrpg.rpg.equipment.EquipmentService;
 import de.pixelrpg.rpg.item.ItemService;
+import de.pixelrpg.rpg.npc.NpcChunkListener;
+import de.pixelrpg.rpg.npc.NpcProtectionListener;
+import de.pixelrpg.rpg.npc.NpcRepository;
+import de.pixelrpg.rpg.npc.NpcRuntimeManager;
+import de.pixelrpg.rpg.npc.YamlNpcRepository;
 import de.pixelrpg.rpg.player.PlayerProfileLifecycleListener;
 import de.pixelrpg.rpg.player.PlayerProfileManager;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public final class PixelRPGPlugin extends JavaPlugin {
     private LifecycleCoordinator lifecycle;
@@ -34,10 +42,21 @@ public final class PixelRPGPlugin extends JavaPlugin {
         GuildCurrencyItemFactory currencyFactory = new GuildCurrencyItemFactory(keys);
         currencyFactory.configureMaxStackSize(getConfig().getInt("economy.currency.max-stack-size", 64));
 
+        ExecutorService npcIo = Executors.newSingleThreadExecutor(runnable -> {
+            Thread thread = new Thread(runnable, "PixelRPG-NpcIO");
+            thread.setDaemon(true);
+            return thread;
+        });
+        NpcRepository npcRepository = new YamlNpcRepository(this, npcIo);
+        NpcRuntimeManager npcRuntime = lifecycle.register(new NpcRuntimeManager(this, keys, npcRepository));
+        npcRuntime.loadAsync();
+
         getServer().getPluginManager().registerEvents(new PlayerProfileLifecycleListener(playerProfileManager), this);
         getServer().getPluginManager().registerEvents(new EquipmentService(this, playerProfileManager), this);
         getServer().getPluginManager().registerEvents(
                 new GuildCurrencyPickupListener(playerProfileManager, playerProfileManager, currencyFactory), this);
+        getServer().getPluginManager().registerEvents(new NpcChunkListener(this, npcRuntime), this);
+        getServer().getPluginManager().registerEvents(new NpcProtectionListener(npcRuntime), this);
     }
 
     @Override
