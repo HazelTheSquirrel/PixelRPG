61937626442c5aff21d8e19b5e92730a5681a992
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-24 04:49:09 +0200

    Wire item service into quest log command

--- src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
+++ src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
@@ -80,7 +80,7 @@ public final class PixelRPGPlugin extends JavaPlugin {
         getLifecycleManager().registerEventHandler(io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents.COMMANDS,
                 event -> {
                     event.registrar().register("pixelrpgparty", new PartyCommand(partyManager, profiles));
-                    event.registrar().register("pixelrpgquestlog", new de.pixelrpg.rpg.quest.QuestLogCommand(questService, profiles));
+                    event.registrar().register("pixelrpgquestlog", new de.pixelrpg.rpg.quest.QuestLogCommand(questService, profiles, itemService));
                 });
         register(StatisticsAPI.class, statisticsService);
         register(ItemAPI.class, itemService);
