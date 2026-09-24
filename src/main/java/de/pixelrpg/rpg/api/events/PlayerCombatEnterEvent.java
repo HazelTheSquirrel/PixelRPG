f24458c60e9f3fe78377676f89799c0238dd3fdd
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-24 04:16:43 +0200

    rebuild: add public API event PlayerCombatEnterEvent.java

---
+++ src/main/java/de/pixelrpg/rpg/api/events/PlayerCombatEnterEvent.java
@@ -0,0 +1,3 @@
+package de.pixelrpg.rpg.api.events;
+import org.bukkit.entity.Player; import org.bukkit.event.Event; import org.bukkit.event.HandlerList;
+public final class PlayerCombatEnterEvent extends Event { private static final HandlerList HANDLERS=new HandlerList(); private final Player player; public PlayerCombatEnterEvent(Player p){player=p;} public Player getPlayer(){return player;} @Override public HandlerList getHandlers(){return HANDLERS;} public static HandlerList getHandlerList(){return HANDLERS;} }
