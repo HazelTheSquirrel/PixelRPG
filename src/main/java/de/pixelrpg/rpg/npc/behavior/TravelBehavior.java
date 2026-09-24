8bef11842d3adb125b803430f8a346fa403ea7ec
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-24 12:56:27 +0200

    Complete NPC behavior parent callback

--- src/main/java/de/pixelrpg/rpg/npc/behavior/TravelBehavior.java
+++ src/main/java/de/pixelrpg/rpg/npc/behavior/TravelBehavior.java
@@ -28,6 +28,12 @@ public final class TravelBehavior implements NpcBehavior {
 
     @Override
     public void onInteract(Player player, RPGNpc npc) {
+        onInteract(player, npc, Player::closeDialog);
+    }
+
+    @Override
+    public void onInteract(Player player, RPGNpc npc, Consumer<Player> backAction) {
+
         if (!profileManager.isRegistered(player.getUniqueId())) {
             dialogueEngine.openNotice(
                     player,
