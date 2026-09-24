2b2a42435ec696a49d9089611e463bdebea14283
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-24 04:24:19 +0200

    rebuild: register food runtime service

--- src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
+++ src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
@@ -5,6 +5,7 @@ import de.pixelrpg.rpg.core.RPGKeys;
 import de.pixelrpg.rpg.api.PartyAPI;
 import de.pixelrpg.rpg.api.StatisticsAPI;
 import de.pixelrpg.rpg.item.ItemService;
+import de.pixelrpg.rpg.item.FoodService;
 import de.pixelrpg.rpg.party.PartyService;
 import de.pixelrpg.rpg.player.PlayerProfileLifecycleListener;
 import de.pixelrpg.rpg.player.PlayerProfileManager;
@@ -18,6 +19,7 @@ public final class PixelRPGPlugin extends JavaPlugin {
     private PartyService partyService;
     private StatisticsService statisticsService;
     private ItemService itemService;
+    private FoodService foodService;
 
     @Override
     public void onEnable() {
@@ -31,7 +33,9 @@ public final class PixelRPGPlugin extends JavaPlugin {
 
         partyService = new PartyService(getConfig().getDouble("quests.party-share-range", 24.0D));
         statisticsService = new StatisticsService();
-        itemService = new ItemService(this);
+        foodService = new FoodService(this);
+        itemService = new ItemService(this, foodService);
+        getServer().getPluginManager().registerEvents(foodService, this);
 
         register(PartyAPI.class, partyService);
         register(StatisticsAPI.class, statisticsService);
@@ -52,6 +56,7 @@ public final class PixelRPGPlugin extends JavaPlugin {
         partyService = null;
         statisticsService = null;
         itemService = null;
+        foodService = null;
     }
 
     private <T> void register(Class<T> type, T service) {
