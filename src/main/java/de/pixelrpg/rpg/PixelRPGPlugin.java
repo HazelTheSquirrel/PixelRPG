387654973125691213e69d57da8f45a1bfbab5b9
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-24 04:41:20 +0200

    rebuild: integrate GUI lifecycle listener

--- src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
+++ src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
@@ -8,6 +8,7 @@ import de.pixelrpg.rpg.equipment.EquipmentService;
 import de.pixelrpg.rpg.api.PartyAPI;
 import de.pixelrpg.rpg.api.StatisticsAPI;
 import de.pixelrpg.rpg.item.ItemService;
+import de.pixelrpg.rpg.gui.GUIListener;
 import de.pixelrpg.rpg.item.FoodService;
 import de.pixelrpg.rpg.party.PartyManager;
 import de.pixelrpg.rpg.party.PartyDisconnectListener;
@@ -65,6 +66,7 @@ public final class PixelRPGPlugin extends JavaPlugin {
         questRepository.load();
         questService = new QuestService(this, questRepository, profiles, itemService);
         getServer().getPluginManager().registerEvents(new QuestLifecycleListener(questService, profiles), this);
+        getServer().getPluginManager().registerEvents(new GUIListener(), this);
         getServer().getPluginManager().registerEvents(foodService, this);
         getServer().getPluginManager().registerEvents(equipmentService, this);
 
