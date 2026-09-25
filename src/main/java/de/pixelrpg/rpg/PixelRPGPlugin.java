ac115c77549db4034f9c1c3a083de21bdefc68ce
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-25 10:04:24 +0200

    Remove story NPC visibility quest tracking

--- src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
+++ src/main/java/de/pixelrpg/rpg/PixelRPGPlugin.java
@@ -91,7 +91,6 @@ import de.pixelrpg.rpg.stats.StatEngine;
 import de.pixelrpg.rpg.stats.StatisticsService;
 import de.pixelrpg.rpg.story.StoryBookFactory;
 import de.pixelrpg.rpg.story.StoryManager;
-import de.pixelrpg.rpg.story.StoryNpcVisibilityListener;
 import de.pixelrpg.rpg.travel.GuildCompassListener;
 import de.pixelrpg.rpg.guild.GuildManager;
 import de.pixelrpg.rpg.region.RegionEditor;
@@ -214,9 +213,6 @@ public final class PixelRPGPlugin extends JavaPlugin {
         npcManager = lifecycle.register(new NpcManager(this));
         npcManager.loadAll();
         getServer().getPluginManager().registerEvents(new NpcChunkListener(npcManager), this);
-        StoryNpcVisibilityListener storyNpcVisibility = new StoryNpcVisibilityListener(this, playerProfileManager, questRepository, storyManager, npcManager);
-        getServer().getPluginManager().registerEvents(storyNpcVisibility, this);
-        lifecycle.register(storyNpcVisibility);
         npcLookTask = new NpcLookTask(this, npcManager, getConfig().getDouble("npc.look-radius", 3.0), getConfig().getDouble("npc.nameplate-radius", 5.0), getConfig().getInt("npc.look-interval-ticks", 5));
         npcLookTask.start();
         DialogueEngine dialogueEngine = new DialogueEngine();
